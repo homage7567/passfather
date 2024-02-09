4d7561ff3aba1c1618152248457257b69b2c79dd
Sergey Panichev <sergeypanichevpm@gmail.com>
2024-02-10 03:20:34 +0700

    Refactoring theme selection

--- apps/passfather/src/app/app.component.ts
+++ apps/passfather/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DropdownModule } from 'primeng/dropdown';
 import { MenubarModule } from 'primeng/menubar';
 import { SharedModule } from 'primeng/api';
 import { FormsModule } from '@angular/forms';
 import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
-import { ThemeOption, ThemeOptionType, ThemeService } from '@pf/theme';
+import { ThemeOption, ThemeService } from '@pf/theme';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -25,24 +25,12 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  public readonly selectedTheme$ = this.themeService.theme$;
+  public readonly themeService = inject(ThemeService);
+  public readonly translocoService = inject(TranslocoService);
+
+  public readonly selectedTheme$ = this.themeService.themeOption$;
   public readonly availableThemes = Object.values(ThemeOption);
 
   public readonly selectedLanguage = this.translocoService.getActiveLang();
   public readonly availableLanguages = this.translocoService.getAvailableLangs();
-
-  constructor(
-    private readonly translocoService: TranslocoService,
-    private readonly themeService: ThemeService
-  ) {
-    this.themeService.init();
-  }
-
-  public onChangeLanguage(languageCode: string): void {
-    this.translocoService.setActiveLang(languageCode);
-  }
-
-  public onChangeTheme(themeOption: ThemeOptionType) {
-    this.themeService.setTheme(themeOption);
-  }
 }
--- libs/theme/src/lib/services/theme.service.ts
+++ libs/theme/src/lib/services/theme.service.ts
@@ -1,53 +1,68 @@
 import { Inject, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, fromEvent, map, Observable, of, shareReplay, startWith, switchMap } from 'rxjs';
 import { Theme, ThemeType } from '../models/theme.enum';
 import { CookieService } from 'ngx-cookie-service';
 import { ThemeOption, ThemeOptionType } from '../models/theme-option.enum';
 import { DOCUMENT } from '@angular/common';
 import { globalConfig } from '@pf/core';
+import { WINDOW } from '@ng-web-apis/common';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
-  public readonly theme$: Observable<ThemeType>;
-
-  private _theme$ = new BehaviorSubject<ThemeType>(Theme.LIGHT);
+  public readonly themeOption$: Observable<ThemeOptionType>;
+  private readonly _themeOption$ = new BehaviorSubject<ThemeOptionType>(this.preloadTheme());
 
   constructor(
+    @Inject(WINDOW) private readonly window: Window,
     @Inject(DOCUMENT) private readonly document: Document,
     private readonly cookieService: CookieService
   ) {
-    this.theme$ = this._theme$.asObservable();
-  }
+    this.themeOption$ = this._themeOption$.asObservable();
 
-  public init(): void {
-    this.preloadCurrentTheme();
+    this.themeOption$
+      .pipe(
+        switchMap(this.getThemeFromThemeOption.bind(this)),
+        shareReplay({ bufferSize: 1, refCount: true }),
+        untilDestroyed(this)
+      )
+      .subscribe(theme => this.setUpTheme(theme));
   }
 
   public setTheme(themeOption: ThemeOptionType): void {
-    this.applyThemeOption(themeOption);
+    this._themeOption$.next(themeOption);
   }
 
-  private preloadCurrentTheme(): void {
-    const currentThemeOption =
-      (this.cookieService.get(globalConfig.cookies.theme) as ThemeOptionType) ||
-      ThemeOption.LIGHT;
-
-    this.applyThemeOption(currentThemeOption);
+  private preloadTheme(): ThemeOptionType {
+    return this.cookieService.check(globalConfig.cookies.theme)
+      ? (this.cookieService.get(globalConfig.cookies.theme) as ThemeOptionType)
+      : ThemeOption.SYSTEM;
   }
 
-  private applyThemeOption(themeOption: ThemeOptionType): void {
-    let theme: ThemeType = Theme.LIGHT;
-
-    if (themeOption === ThemeOption.DARK) {
-      theme = Theme.DARK;
-    }
-
-    this._theme$.next(theme);
+  private getThemeFromThemeOption(themeOption: ThemeOptionType): Observable<ThemeType> {
     this.cookieService.set(globalConfig.cookies.theme, themeOption);
 
-    const themeLink = this.document.getElementById(
-      'app-theme'
-    ) as HTMLLinkElement;
+    switch (themeOption) {
+      case ThemeOption.LIGHT: {
+        return of(Theme.LIGHT);
+      }
+      case ThemeOption.DARK: {
+        return of(Theme.DARK);
+      }
+      default: {
+        const matchMediaColorScheme = this.window.matchMedia('(prefers-color-scheme: dark)');
+
+        return fromEvent(matchMediaColorScheme, 'change').pipe(
+          startWith(matchMediaColorScheme.matches),
+          map(() => (matchMediaColorScheme.matches ? Theme.DARK : Theme.LIGHT))
+        );
+      }
+    }
+  }
+
+  private setUpTheme(theme: ThemeType) {
+    const themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
 
     if (themeLink) {
       themeLink.href = `${theme}.css`;
